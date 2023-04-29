d16922c27b6df42c762287c45cbeb6b6f3614502
RobertoLipovan <lipovan_rob@ieseduardoprimo.es>
2023-04-29 09:35:18 +0000

    Implementación de FASE2

--- ClienteSocket.java
+++ ClienteSocket.java
@@ -3,27 +3,52 @@ import java.net.*;
 import java.util.Scanner;
 
 public class ClienteSocket {
-
-    private static final String DNSAWS = "ec2-44-192-76-21.compute-1.amazonaws.com";
     
+    private static final String DNSAWS = "ec2-44-213-65-61.compute-1.amazonaws.com";
+    private static final int PUERTO = 11000;
+
+    private static boolean isRunning = true;
+
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Socket socket;
-        ObjectInputStream entrada;
-        ObjectOutputStream eixida;
-        String frase;
+        Socket socket = new Socket(DNSAWS, PUERTO);
+        System.out.println("Conexión establecida con el servidor");
 
-        socket = new Socket(DNSAWS, 11000);
-        eixida = new ObjectOutputStream(socket.getOutputStream());
+        ObjectOutputStream salida = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream entrada = new ObjectInputStream(socket.getInputStream());
 
-        System.out.println("Introduce la frase a enviar en minúsculas");
-        Scanner in = new Scanner(System.in);
-        frase = in.nextLine();
-        System.out.println("Se envia la frase " + frase);
-        eixida.writeObject(frase);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese su nombre de usuario: ");
+        String nombreUsuario = scanner.nextLine();
+        salida.writeObject(nombreUsuario);
+        System.out.println(nombreUsuario + ", estás en el chat");
 
-        entrada = new ObjectInputStream(socket.getInputStream());
-        System.out.println(
-                "La frase recibida es: " + (String) entrada.readObject());
-        socket.close();
+        Thread t = new Thread(new ManejadorServidor(entrada));
+        t.start();
+
+        while (isRunning) {
+            String mensaje = scanner.nextLine();
+            salida.writeObject(mensaje);
+        }
+    }
+
+    private static class ManejadorServidor implements Runnable {
+        
+        private ObjectInputStream entrada;
+
+        public ManejadorServidor(ObjectInputStream entrada) {
+            this.entrada = entrada;
+        }
+    
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    String mensaje = (String) entrada.readObject();
+                    System.out.println(mensaje);
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println("Te desconectaste satisfactoriamente");
+            }
+        }
     }
 }
--- ServidorSocket.java
+++ ServidorSocket.java
@@ -1,129 +1,70 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class ServidorSocket {
-    
-   // Declaro un int que contendrá el puerto a usar
-    private static final int PORT = 11000;
-    
-    // Declaro una lista de sockets donde se irán metiendo los clientes
-    private static List<Socket> clients = new ArrayList<>();
+    private static final int PUERTO = 11000;
+    private static List<ObjectOutputStream> clientes = new ArrayList<>();
 
-    // Declaro el main que lanza una IOException
-    public static void main(String[] args) throws IOException {
-        
-        // Declaro un ServerSocket el cual es construido mediante el puerto
-        ServerSocket serverSocket = new ServerSocket(PORT);
-        // Lanzo mensaje para que quede claro desde consola el puerto a usar
-        System.out.println("Server iniciado y escuchando en el puerto " + PORT);
-
-        // Declaro bucle infinito para aceptar clientes y añadirlos a la lista conforme vayan conectandose
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ServerSocket serverSocket = new ServerSocket(PUERTO);
+        System.out.println("Servidor iniciado en el puerto " + PUERTO);
         while (true) {
-            
-            // Declaro un Socket y donde estara el cliente aceptado
-            Socket clientSocket = serverSocket.accept();
-            
-            // Añado ese mismo socket a la lista de clientes
-            clients.add(clientSocket);
-            
-            // Declaro un hilo construido con mi clase estatica creada, la cual necesita un socket
-            Thread thread = new Thread(new ClientHandler(clientSocket));
-            
-            // Lanzo el hilo
-            thread.start();
-            // Lanzo mensaje de confirmacion de conexion y el total de clientes conectados hasta el momento
-            System.out.println("Nuevo cliente conectado. Total de clientes: " + clients.size());
+            Socket socket = serverSocket.accept();
+            System.out.println("Nuevo cliente conectado");
+            ObjectOutputStream salida = new ObjectOutputStream(socket.getOutputStream());
+            clientes.add(salida);
+            Thread t = new Thread(new ManejadorCliente(socket, salida));
+            t.start();
         }
     }
 
-    // Declaro una clase para implementar la interfaz Runnable y que maneje un cliente por hilo
-    static class ClientHandler implements Runnable {
-        
-        // Declaro el Socket cliente
-        private Socket clientSocket;
-        
-        // Declaro objeto stream de entrada
-        private ObjectInputStream entrada;
-        
-        // Declaro objeto stream de salida
-        private ObjectOutputStream eixida;
-
-        // Constructor de clase que necesita de un socket cliente, y lanza una IOException
-        public ClientHandler(Socket clientSocket) throws IOException {
-            
-            // Asigno socket cliente pasado como parametro al constructor
-            this.clientSocket = clientSocket;
-            
-            // Asigno el objeto stream de entrada de ese mismo socket
-            entrada = new ObjectInputStream(clientSocket.getInputStream());
-            
-            // Asigno el objeto stream de salida de ese mismo socket
-            eixida = new ObjectOutputStream(clientSocket.getOutputStream());
-            
-        }
-
-        // Declaro metodo run el cual ejecutará cada hilo, un hilo por cliente
-        public void run() {
-            
+    private static void broadcast(String mensaje) {
+        System.out.println("Mensaje recibido: " + mensaje);
+        for (ObjectOutputStream cliente : clientes) {
             try {
-                
-                while (true) {
-                    
-                    // Declaro un String con la frase del cliente y le asigno el texto que venga por el objeto stream de entrada
-                    String FraseCliente = (String) entrada.readObject();
-                    
-                    // Muestro por consola la dirección del cliente emisor y el mensaje recibido por el mismo
-                    System.out.println("La frase recibida del cliente " + clientSocket.getInetAddress() + " es: " + FraseClient);
-
-                    // Declaro una variable y le asigno ese mismo mensaje recibido pero lo paso a mayusculas todo
-                    String FraseMajuscules = FraseClient.toUpperCase();
-                    
-                    // Muestro por consola el mensaje pasado a mayusculas
-                    System.out.println("El server devuelve la frase en mayúsculas: " + FraseMajuscules);
-                    
-                    // Y envio el mensaje a todos mediante el método enviarMensajeATodos(String mensaje)
-                    enviarMensajeATodos(FraseMajuscules);
-                    
-                }
-                
-            } catch (IOException | ClassNotFoundException e) {
-                
-                e.printStackTrace();
-                
-            } finally {
-                
-                try {
-                    
-                    // Cierro el socket cliente
-                    clientSocket.close();
-                    
-                    // Lo quito de la lista de clientes conectados
-                    clients.remove(clientSocket);
-                    
-                    // Mueestro por consola que un cliente se ha desconectado y muestro también el tamaño de la lista
-                    System.out.println("Cliente desconectado. Total de clientes: " + clients.size());
-                    
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                
+                cliente.writeObject(mensaje);
+            } catch (IOException e) {
+                // Si hay un error al escribir en un ObjectOutputStream, suprimimos la excepción
+                // y continuamos con el siguiente ObjectOutputStream.
             }
         }
+    }
 
-        // Declaro un método para enviar mensaje a todos que necesita de un String el cual sera el mensaje
-        private void enviarMensajeATodos(String mensaje) throws IOException {
-            
-            // Declaro un bucle que recorra la lista de clientes conectados
-            for (Socket client : clients) {
-                
-                // Y si ese cliente no es el emisor del mensaje se le envia el mensaje
-                if (client != clientSocket) {
-                    
-                    // Declaro objeto stream de salida
-                    ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
-                    
-                    // Y escribo el mensaje recibido por el cliente emisor en el objeto stream de salida, el cual deberian leer todos los clientes conectados
-                    out.writeObject(mensaje);
+    private static class ManejadorCliente implements Runnable {
+        private Socket socket;
+        private ObjectOutputStream salida;
+        private ObjectInputStream entrada;
+
+        public ManejadorCliente(Socket socket, ObjectOutputStream salida) throws IOException {
+            this.socket = socket;
+            this.salida = salida;
+            this.entrada = new ObjectInputStream(socket.getInputStream());
+        }
+
+        @Override
+        public void run() {
+            try {
+                String nombreCliente = (String) entrada.readObject();
+                System.out.println(nombreCliente + " se ha unido al chat.");
+                while (true) {
+                    String mensaje = (String) entrada.readObject();
+                    if (mensaje.equalsIgnoreCase("exit")) {
+                        System.out.println(nombreCliente + " se ha desconectado.");
+                        clientes.remove(salida);
+                        broadcast(nombreCliente + " se ha desconectado.");
+                        break;
+                    }
+                    broadcast(nombreCliente + " = " + mensaje);
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println("Cliente desconectado");
+                clientes.remove(salida);
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    // Si hay un error al cerrar el socket, suprimimos la excepción.
                 }
             }
         }
